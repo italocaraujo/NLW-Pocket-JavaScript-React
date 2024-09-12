247b625c4a17f1e18e5363e31a56ea8883c92ed4
Italo Correia <128881501+italocaraujo@users.noreply.github.com>
2024-09-12 11:03:31 -0300

    Add files via upload

--- nlw-pocket-js-react/src/components/create-goal.tsx
+++ nlw-pocket-js-react/src/components/create-goal.tsx
@@ -1,88 +1,152 @@
-import { DialogOverlay, DialogContent, DialogPortal, DialogClose, DialogTitle, DialogDescription } from '@radix-ui/react-dialog'
-import { RadioGroup, RadioGroupItem } from '@radix-ui/react-radio-group'
-import { CheckCircle2, Circle, X } from 'lucide-react'
+import { X } from 'lucide-react'
+import { Button } from './ui/button'
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from './ui/radio-group'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { toast } from 'sonner'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalSchema = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja praticar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalSchema = z.infer<typeof createGoalSchema>
 
 export function CreateGoal() {
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<CreateGoalSchema>({
+    resolver: zodResolver(createGoalSchema),
+  })
+
+  async function handleCreateGoal({
+    title,
+    desiredWeeklyFrequency,
+  }: CreateGoalSchema) {
+    try {
+      await createGoal({
+        title,
+        desiredWeeklyFrequency,
+      })
+
+      reset()
+
+      queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+      queryClient.invalidateQueries({ queryKey: ['summary'] })
+
+      toast.success('Meta criada com sucesso!')
+    } catch {
+      toast.error('Erro ao criar a meta, tente novamente!')
+    }
+  }
+
   return (
-    <DialogPortal>
-      <DialogOverlay className="fixed inset-0 z-40 bg-black/40 backdrop-blur-sm" />
+    <DialogContent>
+      <div className="flex flex-col gap-6 h-full">
+        <div className="space-y-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
 
-      <DialogContent className="fixed z-50 right-0 top-0 bottom-0 w-[400px] h-screen border-l border-zinc-900 bg-zinc-950 flex flex-col justify-between p-8">
-        <div className="space-y-6">
-          <div className="space-y-3">
-            <div className="flex items-center justify-between">
-              <DialogTitle className="text-lg font-semibold">Cadastrar meta</DialogTitle>
-
-              <DialogClose>
-                <X className="size-5 text-zinc-600" />
-              </DialogClose>
-            </div>
-
-            <DialogDescription className="text-zinc-400 text-sm leading-relaxed">
-              Adicione atividades que te fazem bem e que você quer continuar praticando toda semana.
-            </DialogDescription>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
+            </DialogClose>
           </div>
 
-          <form action="" className="space-y-6">
-            <div className="flex flex-col gap-2">
-              <label 
-                htmlFor="title"
-                className="font-medium text-sm tracking-tight leading-normal" 
-              >
-                Qual a atividade?
-              </label>
+          <DialogDescription>
+            Adicione atividades que te fazem bem e que você quer continuar
+            praticando toda semana.
+          </DialogDescription>
+        </div>
 
-              <input 
-                type="text" 
-                name="title" 
-                id="title"  
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
+          <div className="space-y-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Qual a atividade?</Label>
+
+              <Input
+                id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc..."
-                className="px-4 h-12 bg-black border border-zinc-900 rounded-lg placeholder-zinc-400 outline-none text-sm hover:border-zinc-800 focus-visible:border-pink-500 focus-visible:ring-4 ring-pink-500/10"
+                {...register('title')}
               />
+
+              {errors.title && (
+                <p className="text-sm text-red-400">{errors.title.message}</p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
-              <label 
-                htmlFor="desiredWeeklyFrequency"
-                className="font-medium text-sm tracking-tight leading-normal" 
-              >
+              <Label htmlFor="desiredWeeklyFrequency">
                 Quantas vezes na semana?
-              </label>
+              </Label>
 
-              <RadioGroup id="desiredWeeklyFrequency" className="flex flex-col gap-2">
-                {Array.from({ length: 7 }).map((_, i) => {
-                  const frequency = String(i + 1)
-
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={5}
+                render={({ field }) => {
                   return (
-                    <RadioGroupItem 
-                      key={i}
-                      value={frequency} 
-                      className="group bg-black border border-zinc-900 rounded-lg px-4 py-2.5 flex items-center justify-between outline-none hover:border-zinc-800 focus-visible:border-pink-500 focus-visible:ring-4 ring-pink-500/10 data-[state=checked]:bg-pink-500/5 data-[state=checked]:border-pink-500"
+                    <RadioGroup
+                      value={String(field.value)}
+                      onValueChange={field.onChange}
                     >
-                      <Circle className="size-4 text-zinc-600 group-data-[state=checked]:hidden" />
-                      <CheckCircle2 className="size-4 text-pink-500 hidden group-data-[state=checked]:inline" />
+                      {Array.from({ length: 7 }).map((_, i) => {
+                        const frequency = String(i + 1)
 
-                      <span className="text-zinc-300 text-sm font-medium leading-none">{frequency}x na semana</span>
-                      <span className="text-lg leading-none">🥱</span>
-                    </RadioGroupItem>
+                        return (
+                          <RadioGroupItem key={i} value={frequency}>
+                            <RadioGroupIndicator />
+                            <span className="text-zinc-300 text-sm font-medium leading-none">
+                              {frequency}x na semana
+                            </span>
+                            <span className="text-lg leading-none">🥱</span>
+                          </RadioGroupItem>
+                        )
+                      })}
+                    </RadioGroup>
                   )
-                })}
-              </RadioGroup>
+                }}
+              />
             </div>
-          </form>
-        </div>
+          </div>
 
-        <div className="flex items-center gap-3">
-          <DialogClose className="flex-1 flex items-center justify-center gap-2 px-4 py-2.5 rounded-lg bg-zinc-900 text-zinc-400 text-sm font-medium tracking-tight hover:bg-zinc-800">
-            Fechar
-          </DialogClose>
+          <div className="flex items-center gap-3 mt-auto">
+            <DialogClose asChild>
+              <Button variant="secondary" className="flex-1">
+                Fechar
+              </Button>
+            </DialogClose>
 
-          <button className="flex-1 flex items-center justify-center gap-2 px-4 py-2.5 rounded-lg bg-violet-500 text-indigo-50 text-sm font-medium tracking-tight hover:bg-violet-600">
-            Salvar
-          </button>
-        </div>
-      </DialogContent>
-    </DialogPortal>
+            <Button type="submit" className="flex-1">
+              Salvar
+            </Button>
+          </div>
+        </form>
+      </div>
+    </DialogContent>
   )
 }
--- nlw-pocket-js-react/src/components/weekly-summary.tsx
+++ nlw-pocket-js-react/src/components/weekly-summary.tsx
@@ -1,130 +1,99 @@
-import { CheckCircle2, Plus } from "lucide-react";
-import { InOrbitIcon } from "./in-orbit-icon";
-import { DialogTrigger } from "@radix-ui/react-dialog";
-import { Progress, ProgressIndicator } from "@radix-ui/react-progress";
+import { CheckCircle2, Plus } from 'lucide-react'
+import { InOrbitIcon } from './in-orbit-icon'
+import { DialogTrigger } from '@radix-ui/react-dialog'
+import { Button } from './ui/button'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
+import type { GetSummaryResponse } from '../http/get-summary'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-BR'
+import { PendingGoals } from './pending-goals'
 
-export function WeeklySummary() {
+dayjs.locale(ptBR)
+
+interface WeeklySummaryProps {
+  summary: GetSummaryResponse['summary']
+}
+
+export function WeeklySummary({ summary }: WeeklySummaryProps) {
+  const fromDate = dayjs().startOf('week').format('D[ de ]MMM')
+  const toDate = dayjs().endOf('week').format('D[ de ]MMM')
+
+  const completedPercentage = Math.round(
+    (summary.completed * 100) / summary.total
+  )
+
   return (
-    <main className="max-w-[480px] py-10 px-5 mx-auto flex flex-col gap-6">
+    <main className="max-w-[540px] py-10 px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           <InOrbitIcon />
-          <span className="text-lg font-semibold">05 a 12 de Agosto</span>
+          <span className="text-lg font-semibold">
+            {fromDate} - {toDate}
+          </span>
         </div>
 
-        <DialogTrigger className="flex items-center gap-2 px-3 py-1.5 rounded-lg bg-violet-500 text-indigo-50 text-sm font-medium tracking-tight hover:bg-violet-600">
-          <Plus className="size-4" />
-          Cadastrar meta
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
         </DialogTrigger>
       </div>
 
       <div className="flex flex-col gap-3">
-        <Progress value={50} max={100} className="bg-zinc-900 rounded-full h-2">
-          <ProgressIndicator className="bg-gradient-to-r from-pink-500 to-violet-500 w-1/2 h-2 rounded-full" />
+        <Progress value={summary.completed} max={summary.total}>
+          <ProgressIndicator style={{ width: `${completedPercentage}%` }} />
         </Progress>
 
         <div className="flex items-center justify-between text-xs text-zinc-400">
-          <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
-          <span>58%</span>
+          <span>
+            Você completou{' '}
+            <span className="text-zinc-100">{summary.completed}</span> de{' '}
+            <span className="text-zinc-100">{summary.total}</span> metas nessa
+            semana.
+          </span>
+          <span>{completedPercentage}%</span>
         </div>
       </div>
 
-      <div className="h-px bg-zinc-900" />
+      <Separator />
 
-      <div className="flex flex-wrap gap-3">
-        <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-          <Plus className="size-4 text-zinc-600" />
-          Meditar
-        </button>
-
-        <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-          <Plus className="size-4 text-zinc-600" />
-          Praticar exercício
-        </button>
-
-        <button className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700">
-          <Plus className="size-4 text-zinc-600" />
-          Levantar cedo
-        </button>
-
-        <button 
-          disabled
-          className="flex items-center px-3 py-2 gap-2 leading-none rounded-full border border-dashed border-zinc-800 text-sm text-zinc-300 hover:border-zinc-700 disabled:opacity-50 disabled:pointer-events-none"
-        >
-          <Plus className="size-4 text-zinc-600" />
-          Manter uma boa alimentação
-        </button>
-      </div>
+      <PendingGoals />
 
       <div className="space-y-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
-        <div className="space-y-4">
-          <h3 className="font-medium">Hoje <span className="text-zinc-400 text-xs">(06 de agosto)</span></h3>
 
-          <ul className="space-y-3">
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Acordar cedo</span>" às <span className="text-zinc-100">08:13h</span>
-              </span>
-              <button className="underline text-zinc-500 text-xs hover:text-zinc-400">
-                Desfazer
-              </button>
-            </li>
+        {Object.entries(summary.goalsPerDay).map(([date, goals]) => {
+          const weekDay = dayjs(date).format('dddd')
+          const parsedDate = dayjs(date).format('D[ de ]MMM')
 
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Meditar</span>" às <span className="text-zinc-100">08:13h</span>
-              </span>
-              <button className="underline text-zinc-500 text-xs hover:text-zinc-400">
-                Desfazer
-              </button>
-            </li>
+          return (
+            <div className="space-y-4" key={date}>
+              <h3 className="font-medium capitalize">
+                {weekDay}{' '}
+                <span className="text-zinc-400 text-xs">({parsedDate})</span>
+              </h3>
 
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Praticar exercício</span>" às <span className="text-zinc-100">08:13h</span>
-              </span>
-              <button className="underline text-zinc-500 text-xs hover:text-zinc-400">
-                Desfazer
-              </button>
-            </li>
-          </ul>
-        </div>
+              <ul className="space-y-3">
+                {goals.map(goal => {
+                  const parsedTime = dayjs(goal.createdAt).format('HH:mm[h]')
 
-        <div className="space-y-4">
-          <h3 className="font-medium">Ontem <span className="text-zinc-400 text-xs">(05 de agosto)</span></h3>
-
-          <ul className="space-y-3">
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Acordar cedo</span>" às <span className="text-zinc-100">08:13h</span>
-              </span>
-              <button className="underline text-zinc-500 text-xs hover:text-zinc-400">
-                Desfazer
-              </button>
-            </li>
-          </ul>
-        </div>
-
-        <div className="space-y-4">
-          <h3 className="font-medium">Quarta-feira <span className="text-zinc-400 text-xs">(04 de agosto)</span></h3>
-
-          <ul className="space-y-3">
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Acordar cedo</span>" às <span className="text-zinc-100">08:13h</span>
-              </span>
-              <button className="underline text-zinc-500 text-xs hover:text-zinc-400">
-                Desfazer
-              </button>
-            </li>
-          </ul>
-        </div>
+                  return (
+                    <li className="flex items-center gap-2" key={goal.id}>
+                      <CheckCircle2 className="size-4 text-pink-500" />
+                      <span className="text-sm text-zinc-400">
+                        Você completou "
+                        <span className="text-zinc-100">{goal.title}</span>" às{' '}
+                        <span className="text-zinc-100">{parsedTime}</span>
+                      </span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })}
       </div>
     </main>
   )
